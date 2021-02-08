8611d463690d69a0fa614b9a9fdd7ec9db1f43f2
finne0528 <kazuki.mineshima@it-finne.com>
2021-02-08 19:16:51 +0900

    commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,8 @@ plugins {
 	id("io.spring.dependency-management") version "1.0.10.RELEASE"
 	kotlin("jvm") version "1.4.10"
 	kotlin("plugin.spring") version "1.4.10"
+
+	application
 }
 
 group = "com.dailyreport"
